28606c8b1defc37644c5241e36cf8ba97ec2ecbd
Vasty <vxstydev@gmail.com>
2024-12-18 10:21:17 -0300

    feat: minimal change of executor colors

--- src/main/java/dev/club/aquatic/command/Executor.java
+++ src/main/java/dev/club/aquatic/command/Executor.java
@@ -1,6 +1,7 @@
 package dev.club.aquatic.command;
 
 import dev.club.aquatic.AuthCore;
+import dev.club.aquatic.utils.ColorUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -22,23 +23,23 @@ public class Executor implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!sender.hasPermission("authcore.admin")) {
-            sender.sendMessage("§c[AuthCore] You do not have permission to use this command.");
+            sender.sendMessage(ColorUtils.Set("&c[AuthCore] You do not have permission to use this command."));
             return true;
         }
 
         if (args.length == 0) {
-            sender.sendMessage("§b[AuthCore] Use: /authcore <settings|events|reload>");
+            sender.sendMessage(ColorUtils.Set("&b[AuthCore] Use: /authcore <settings|events|reload>"));
             return true;
         }
 
         if (args[0].equalsIgnoreCase("reload")) {
             AuthCore.SetConfig().reload();
-            sender.sendMessage("§a[AuthCore] Configuration has been reloaded.");
+            sender.sendMessage(ColorUtils.Set("&a[AuthCore] Configuration has been reloaded."));
             return true;
         }
 
         if (args.length < 3) {
-            sender.sendMessage("§3[AuthCore] Use: /authcore <settings|events> <key> <true|false>");
+            sender.sendMessage(ColorUtils.Set("&3[AuthCore] Use: /authcore <settings|events> <key> <true|false>"));
             return true;
         }
 
@@ -47,7 +48,7 @@ public class Executor implements CommandExecutor {
         String value = args[2].toLowerCase();
 
         if (!value.equals("true") && !value.equals("false")) {
-            sender.sendMessage("§c[AuthCore] The value must be 'true' or 'false'.");
+            sender.sendMessage(ColorUtils.Set("&c[AuthCore] The value must be 'true' or 'false'."));
             return true;
         }
 
@@ -57,16 +58,16 @@ public class Executor implements CommandExecutor {
         if (category.equals("settings") || category.equals("events")) {
             String path = category + "." + key;
             if (!config.contains(path)) {
-                sender.sendMessage("§c[AuthCore] The key '" + key + "' does not exist in " + category + ".");
+                sender.sendMessage(ColorUtils.Set("&c[AuthCore] The key '" + key + "' does not exist in " + category + "."));
                 return true;
             }
 
             config.set(path, booleanValue);
             plugin.saveConfig();
             plugin.reloadConfig();
-            sender.sendMessage("§a[AuthCore] The key '" + key + "' in " + category + " has been updated to " + value + ".");
+            sender.sendMessage(ColorUtils.Set("&a[AuthCore] The key '" + key + "' in " + category + " has been updated to " + value + "."));
         } else {
-            sender.sendMessage("§c[AuthCore] Unknown category. Use 'settings' or 'events'.");
+            sender.sendMessage(ColorUtils.Set("&c[AuthCore] Unknown category. Use 'settings' or 'events'."));
         }
 
         return true;
